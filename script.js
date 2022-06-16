e8c22eebcee5c0dbc1e5bfeed265f8dbe4e516ce
saff-it <saff_it@ymail.com>
2022-06-16 16:06:06 +0200

    milestone 1 attempt: Fizz does not overwrite number in console

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-for ( let i = 0; i <= 100 ; i++){
-    console.log(i);
+
+
+for ( let index = 0; index <= 100 ; index++) {
+    console.log(index);
+
+    // let specialWord = index;
+
+    if (index % 3 == 0) {
+        console.log('Fizz');
+
+    } else if (index % 5 == 0) {
+        console.log('Buzz');
+
+    }
 }
 
 
