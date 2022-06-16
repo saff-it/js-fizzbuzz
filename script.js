6ca632f7902eed654de66e566a89b7450cb7e745
saff-it <saff_it@ymail.com>
2022-06-16 16:31:22 +0200

    Milestone 1 completed

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 
+for ( let index = 1; index <= 100 ; index++) {
+    
 
-
-for ( let index = 0; index <= 100 ; index++) {
-    console.log(index);
-
-    // let specialWord = index;
-
-    if (index % 3 == 0) {
+    if (index % 3 == 0 && index % 5 == 0) {
+        console.log('FizzBuzz');
+    
+    } else if (index % 3 == 0) {
         console.log('Fizz');
 
     } else if (index % 5 == 0) {
         console.log('Buzz');
 
+    } else {
+        console.log(index);
     }
+   
 }
-
-
-
 
 
 
