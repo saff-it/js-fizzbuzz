0c07d6239484706f151cd9730faddf3cc0a14383
saff-it <saff_it@ymail.com>
2022-06-16 18:01:44 +0200

    bonus 1 improvements

--- script.js
+++ script.js
@@ -5,24 +5,21 @@ for ( let index = 1; index <= 100 ; index++) {
      let message;
 
     if (index % 3 == 0 && index % 5 == 0) {
-        console.log('FizzBuzz');
         message = "FizzBuzz";
 
     
     } else if (index % 3 == 0) {
-        console.log('Fizz');
         message = "Fizz";
 
     } else if (index % 5 == 0) {
-        console.log('Buzz');
         message = "Buzz";
 
-
     } else {
-        console.log(index);
         message = index;
 
     }
+    console.log('FizzBuzz', 'Fizz', 'Buzz', index);
+
 
     const showIndex = document.createElement('div');
     showIndex.classList.add('col-2');
