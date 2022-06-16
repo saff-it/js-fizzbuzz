e793ac214595e11d07c77b195fec197b4511f33a
saff-it <saff_it@ymail.com>
2022-06-16 18:01:24 +0200

    bonus 1 completed

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ const rowShow = document.getElementById('html-show')
 
 for ( let index = 1; index <= 100 ; index++) {
     
-     
+     let message;
 
     if (index % 3 == 0 && index % 5 == 0) {
         console.log('FizzBuzz');
+        message = "FizzBuzz";
+
     
     } else if (index % 3 == 0) {
         console.log('Fizz');
+        message = "Fizz";
 
     } else if (index % 5 == 0) {
         console.log('Buzz');
+        message = "Buzz";
+
 
     } else {
         console.log(index);
+        message = index;
+
     }
 
     const showIndex = document.createElement('div');
     showIndex.classList.add('col-2');
-    showIndex.innerHTML = index;
+    showIndex.innerHTML = message;
 
 
     rowShow.append(showIndex);
